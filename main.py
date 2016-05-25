92376612d8979b83c6f0da10072c159e3fdab686
Ale Sanchez <alesanchezmedina@gmail.com>
2016-05-25 19:14:05 +0200

    Todo

--- main.py
+++ main.py
@@ -7,23 +7,32 @@ from scipy.io.wavfile import write
 
 import RPi.GPIO as GPIO
 
-sd.default.device = 4
+sd.default.device = 2
+channels = sd.query_devices()[sd.default.device[0]]['max_input_channels']
 fs = 48000
 duration = 10  # seconds
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.add_event_detect(23, GPIO.FALLING)
 GPIO.add_event_detect(24, GPIO.RISING)
 
 while True:
-    myrecording = np.zeros((0, 2))
+    myrecording = np.zeros((0, channels))
     
-    GPIO.wait_for_edge(23, GPIO.FALLING)
-    with sd.InputStream(channels=2) as in_stream:
+    print("Listo")
+    while not GPIO.event_detected(23):
+        pass
+
+
+    with sd.InputStream(channels=channels) as in_stream:
+        print("Uno")
+        sd.sleep(1000)
+        print("dos")
         t = time.time()
         while time.time() - t <= duration and not GPIO.event_detected(24):
             myrecording = np.append(myrecording, in_stream.read(384)[0], axis=0)
         
     write('product.wav', fs, myrecording)
     print(transcript.run())
-        
