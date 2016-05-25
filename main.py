a1d0a3c2ae915ffd562edc9a2d520fdb7b9dd3ef
Ale Sanchez <alesanmed@alum.us.es>
2016-05-25 14:08:07 +0200

    A

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
-import numpy as np
-import RPi.GPIO as GPIO
 import time
-
+import numpy as np
+import sounddevice as sd
 import tools.transcript as transcript
-import tools.record as record
 
 from scipy.io.wavfile import write
 
+import RPi.GPIO as GPIO
+
+sd.default.device = 4
+fs = 48000
+duration = 10  # seconds
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.add_event_detect(24, GPIO.RISING)
 
-out = np.empty((48000*10, 1))
-
-print("Empieza")
 while True:
-	GPIO.wait_for_edge(23, GPIO.FALLING)
-	record.run(out)
-	GPIO.wait_for_edge(23, GPIO.RISING)
-	record.stop()
-	write("product.wav", 48000, out)
+    myrecording = np.zeros((0, 2))
+    
+    GPIO.wait_for_edge(23, GPIO.FALLING)
+    with sd.InputStream(channels=2) as in_stream:
+        t = time.time()
+        while time.time() - t <= duration and not GPIO.event_detected(24):
+            myrecording = np.append(myrecording, in_stream.read(384)[0], axis=0)
+        
+    write('product.wav', fs, myrecording)
+    print(transcript.run())
+        
