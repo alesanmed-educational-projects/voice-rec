51c6e585e14846888b367ed8e8f00612cfce94ec
practicas <practicas@ATC-118.(none)>
2016-05-30 12:18:06 +0200

    Multiprocesamiento

--- main.py
+++ main.py
@@ -1,48 +1,20 @@
-import time
-import numpy as np
-import sounddevice as sd
-import tools.transcript as transcript
-import pygame
+from multiprocessing import Process
+import tools.barcode as barcode
 
-from scipy.io.wavfile import write
+def runInParallel(*fns):
+	proc = []
+	for fn in fns:
+		p = Process(target=fn)
+		p.start()
+		proc.append(p)
+	for p in proc:
+		p.join()
 
-import RPi.GPIO as GPIO
+def run_barcode():
+	barcode.run()
 
-sd.default.device = 2
-channels = sd.query_devices()[sd.default.device[0]]['max_input_channels']
-fs = 48000
-duration = 10  # seconds
+def run_voice():
+	pass
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.add_event_detect(23, GPIO.FALLING)
-GPIO.add_event_detect(24, GPIO.RISING)
-
-pygame.mixer.init()
-pygame.mixer.music.load('alert.wav')
-
-while True:
-    myrecording = np.zeros((0, channels))
-    
-    print("Listo")
-    while not GPIO.event_detected(23):
-        pass
-
-    in_stream = sd.InputStream(channels=channels)
-    sd.sleep(1000)
-    print("Comienza a hablar")
-    pygame.mixer.music.play()
-    
-    while pygame.mixer.music.get_busy() == True:
-        continue
-
-    t = time.time()
-    while time.time() - t <= duration and not GPIO.event_detected(24):
-        myrecording = np.append(myrecording, in_stream.read(384)[0], axis=0)
-    
-    print("Traduciendo")
-    in_stream.stop()
-
-    write('product.wav', fs, myrecording)
-    print(transcript.run())
+if __name__ == '__main__':
+	runInParallel(run_barcode, run_voice)
--- tools/barcode.py
+++ tools/barcode.py
@@ -1,19 +1,22 @@
 import json
 import os.path
+import os
+import sys
 
 def run():
+	sys.stdin = open(0)
 	while (True):
-	filepath = 'barcodes.json'
+		filepath = 'barcodes.json'
 
-	if os.path.exists(filepath):
-		with open(filepath, 'r') as outfile:
-			barcodes = json.load(outfile)['barcodes']
-	else:
-		barcodes = []
+		if os.path.exists(filepath):
+			with open(filepath, 'r') as outfile:
+				barcodes = json.load(outfile)['barcodes']
+		else:
+			barcodes = []
 
-	barcode = input()
-	if barcode:
-		barcodes.append(barcode)
+		barcode = input()
+		if barcode:
+			barcodes.append(barcode)
 
-	with open('barcodes.json', 'w') as outfile:
-		json.dump({'barcodes': barcodes }, outfile)
+		with open('barcodes.json', 'w') as outfile:
+			json.dump({'barcodes': barcodes }, outfile)
