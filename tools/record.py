fc37f479efa363a3482c70023df8f75b90a98329
Ale Sanchez <alesanmed@alum.us.es>
2016-05-30 14:03:44 +0200

    Pyaudio

--- tools/record.py
+++ tools/record.py
@@ -1,3 +1,20 @@
+from ctypes import *
+from contextlib import contextmanager
+
+ERROR_HANDLER_FUNC = CFUNCTYPE(None, c_char_p, c_int, c_char_p, c_int, c_char_p)
+
+def py_error_handler(filename, line, function, err, fmt):
+    pass
+
+c_error_handler = ERROR_HANDLER_FUNC(py_error_handler)
+
+@contextmanager
+def noalsaerr():
+    asound = cdll.LoadLibrary('libasound.so')
+    asound.snd_lib_error_set_handler(c_error_handler)
+    yield
+    asound.snd_lib_error_set_handler(None)
+
 import json
 import RPi.GPIO as GPIO
 import os
@@ -25,7 +42,9 @@ def record():
     pygame.mixer.init()
     pygame.mixer.music.load('alert.wav')   
     
-    audio = pyaudio.PyAudio()
+    with noalsaerr():
+        audio = pyaudio.PyAudio()
+
     for i in range(audio.get_device_count()):
         device = audio.get_device_info_by_index(i)
 
