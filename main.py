03f4610e8597bb8ff8d0ec27acc6d9572ba9fdef
Ale Sanchez <alesanmed@alum.us.es>
2016-05-30 13:14:06 +0200

    Main nuevo

--- main.py
+++ main.py
@@ -1,21 +1,33 @@
-from multiprocessing import Process
+from multiprocessing import Process, Queue
 import tools.barcode as barcode
 import tools.record as record
 
-def runInParallel(*fns):
-	proc = []
-	for fn in fns:
-		p = Process(target=fn)
-		p.start()
-		proc.append(p)
-	for p in proc:
-		p.join()
+def runInParallel(q, *fns):
+    
+    proc = []
+    for fn in fns:
+        p = Process(target=fn, args=(q,))
+        p.start()
+        proc.append(p)
+    for p in proc:
+        p.join()
+    
+    return proc
 
 def run_barcode():
-	barcode.run()
+    barcode.run()
 
-def run_voice():
-	record.run()
+def run_voice(q):
+    result = record.run()
+    if result == -1:
+        q.put(-1)
 
 if __name__ == '__main__':
-	runInParallel(run_barcode, run_voice)
+    first = True
+    while True:
+        if first:
+            q = Queue() 
+            proc = runInParallel(q, run_barcode, run_voice)
+            first = False
+        
+        print(q.get())
